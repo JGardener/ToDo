38a5da3f98a3c46fb864cb9fd854c638ab1d8c9a
James Gardener <jamesgardener92@gmail.com>
2022-02-08 09:10:51 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+
+function App() {
+  return(
+    <main>
+      <Header />
+      <List />
+    </main>
+  )
+}
+
+export default App;
---
+++ src/Components/List/List.js
@@ -0,0 +1,9 @@
+const List = () => {
+    return(
+        <section className={"list"} >
+            <p>This is the list container</p>
+        </section>
+    )
+}
+
+export default List;
